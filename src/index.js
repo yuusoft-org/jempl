99590b235473fd879b4c26ef8c6fd6cadbf3fc73
Luciano Hanyon Wu <han4wluc@yuusoft.com>
2026-04-29 17:03:37 +0800

    Add public condition evaluator (#64)

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import render from "./render.js";
 import parse from "./parse/index.js";
 import parseAndRender from "./parseAndRender.js";
+import evaluateCondition from "./evaluateCondition.js";
 import { parseConditionExpression, parseConditionJson } from "./parse/utils.js";
 
 export {
   render,
   parseAndRender,
   parse,
+  evaluateCondition,
   parseConditionExpression,
   parseConditionJson,
 };
